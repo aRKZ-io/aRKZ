af313f7aabaa8a46ec2788ac6e6526a065425347
aRKZ-io <122447937+aRKZ-io@users.noreply.github.com>
2023-11-03 23:57:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ let my = document.querySelectorAll('.navbar');
 function roll () {
 }
 
+function scrollToSection() {
+    //Getting reference to the page I want to scroll to 
+    const section = document.getElementById ('banner2')
+
+    //Scrolling selection
+    section.scrollIntoView ({ behavior:"smooth" })
+}
+
+function scrollToSectio() {
+    //Getting reference to the page I want to scroll to 
+    const section = document.getElementById ('banner4')
+
+    //Scrolling selection
+    section.scrollIntoView ({ behavior:"smooth" })
+}
+
     
